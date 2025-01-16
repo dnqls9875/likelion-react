d839e3e86d6f51fe5a878973cf29d15486caadd2
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2025-01-16 10:33:42 +0900

    [실습] 조건부 렌더링 vs 조건부 디스프레이
    - 렌더링 비용
    - 전환 비용
    - 간단한 상태 전환 시 전환 비용은 조건부 디스플레이가 효율적

--- frontend/components/switch-list.tsx
+++ frontend/components/switch-list.tsx
@@ -10,16 +10,41 @@ const switchStyles = {
   display: "flex",
   flexFlow: "column",
   gap: 12,
+  listStyle: "none",
+  paddingInlineStart: 0,
 };
 
+// 리스트 렌더링 02
+// Array.prototype.map (O) : 새로운 배열 반환 (반환값이 있으므로 리엑트에서 적절하다.)
+// Array.prototype.forEach (X) : undefined (반환값이 없으므로 리엑트에서 사용하지 않는다.)
+
 function SwitchList({ items }: SwitchListProps) {
-  // 리스트 렌더링 02
-  // Array.prototype.map (O) : 새로운 배열 반환 (반환값이 있으므로 리엑트에서 적절하다.)
-  // Array.prototype.forEach (X) : undefined (반환값이 없으므로 리엑트에서 사용하지 않는다.)
+  // React 컴포넌트 상태 관리
+  // React Hooks API (React.useState)
+  // 어떤 상태? (과제 제출)
+
+  const [submission, setSubmission] = React.useState(false);
+
+  function switchSubmission() {
+    const switchSubmission = !submission;
+  }
+
   return (
     <ul className="SwitchList" style={switchStyles}>
-      {items.map((item, index) => (
-        <li key={index}>
+      <li>
+        <Switch
+          active={submission}
+          onToggle={() => {
+            const switchSubmission = !submission;
+            setSubmission(switchSubmission);
+          }}
+        >
+          과제 제출
+        </Switch>
+      </li>
+      {/* 조건부 디스플레이 (Conditional Display) */}
+      {items.map((item) => (
+        <li key={item.id} style={{ display: "none" }}>
           <Switch
             active={item.active}
             disabled={item.disabled}
@@ -30,6 +55,21 @@ function SwitchList({ items }: SwitchListProps) {
           </Switch>
         </li>
       ))}
+
+      {/* 조건부 렌더링 (Conditional Rendering) */}
+      {false &&
+        items.map((item) => (
+          <li key={item.id}>
+            <Switch
+              active={item.active}
+              disabled={item.disabled}
+              showOnOffText={item.showOnOffText}
+              onToggle={item.onToggle}
+            >
+              {item.children}
+            </Switch>
+          </li>
+        ))}
     </ul>
   );
 }
